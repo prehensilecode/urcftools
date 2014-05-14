701cc77aed2029c2583faffa39bc5db2ec0ca847
David Chin <david.w.h.chin@gmail.com>
2014-05-14 17:07:43 -0400

    Add resource reservation for jobs needing >1 node.

--- pecheck_simple.go
+++ pecheck_simple.go
@@ -6,6 +6,9 @@ package main
 
 import (
     "github.com/dgruber/jsv"
+    "strings"
+    "strconv"
+
 )
 
 func jsv_on_start_function() {
@@ -25,12 +28,33 @@ func job_verification_function() {
         jsv.JSV_set_param("binding_amount", "1")
         jsv.JSV_set_param("binding_exp_n", "0")
         modified_p = true
+    } else {
+        hostlist, _ := jsv.JSV_get_param("q_hard")
+        hostlist = strings.SplitAfterN(hostlist, "@", 2)[1]
+
+        v, _ := jsv.JSV_get_param("pe_max")
+        pe_max, _ := strconv.Atoi(v)
+
+        if strings.EqualFold("@amdhosts", hostlist) {
+            if pe_max > 127 {
+                jsv.JSV_set_param("R", "y")
+                modified_p = true
+            }
+        } 
+
+        if strings.EqualFold("@intelhosts", hostlist) {
+            if pe_max > 31 {
+                jsv.JSV_set_param("R", "y")
+                modified_p = true
+            }
+        }
     }
 
     // show qsub params
     //jsv.JSV_show_params()
 
     if modified_p {
+        jsv.JSV_show_params()
         jsv.JSV_correct("Job was modified")
     } else {
         jsv.JSV_correct("Job was not modified")
