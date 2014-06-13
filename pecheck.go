2ad3630a5f914b050887cbfa04ed05c6041c4a5b
David Chin <david.w.h.chin@gmail.com>
2014-06-13 19:42:36 -0400

    Deal with the case where a variable number of slots is requested -- do not set any binding in that case.

--- pecheck.go
+++ pecheck.go
@@ -31,46 +31,54 @@ func job_verification_function() {
     } else {
         if !jsv.JSV_is_param("binding_strategy") {
             var pe_max int
+            var pe_min int
             var v string
             v, _ = jsv.JSV_get_param("pe_max")
             pe_max, _ = strconv.Atoi(v)
+
+            v, _ = jsv.JSV_get_param("pe_min")
+            pe_min, _ = strconv.Atoi(v)
 
             var hostlist string 
             hostlist, _ = jsv.JSV_get_param("q_hard")
             hostlist = strings.SplitAfterN(hostlist, "@", 2)[1]
 
-            jsv.JSV_set_param("binding_strategy", "striding_automatic")
-            jsv.JSV_set_param("binding_type", "pe")
+            // Only specify binding if pe_max == pe_min, i.e. if a fixed
+            // number of slots is requested
 
-            if strings.EqualFold("@intelhosts", hostlist) {
-                if pe_max < intel_slots {
-                    jsv.JSV_set_param("binding_amount", strconv.Itoa(pe_max))
-                } else {
-                    jsv.JSV_set_param("binding_amount", strconv.Itoa(intel_slots))
-                }
+            if pe_max == pe_min {
+                jsv.JSV_set_param("binding_strategy", "striding_automatic")
+                jsv.JSV_set_param("binding_type", "pe")
 
-                if pe_max > 31 {
+                if strings.EqualFold("@intelhosts", hostlist) {
+                    if pe_max < intel_slots {
+                        jsv.JSV_set_param("binding_amount", strconv.Itoa(pe_max))
+                    } else {
+                        jsv.JSV_set_param("binding_amount", strconv.Itoa(intel_slots))
+                    }
+
+                    if pe_max > 31 {
                     jsv.JSV_set_param("R", "y")
-                }
-            } else if strings.EqualFold("@amdhosts", hostlist) {
-                if pe_max < amd_slots {
-                    jsv.JSV_set_param("binding_amount", strconv.Itoa(pe_max))
-                } else {
-                    jsv.JSV_set_param("binding_amount", strconv.Itoa(amd_slots))
+                    }
+                } else if strings.EqualFold("@amdhosts", hostlist) {
+                    if pe_max < amd_slots {
+                        jsv.JSV_set_param("binding_amount", strconv.Itoa(pe_max))
+                    } else {
+                        jsv.JSV_set_param("binding_amount", strconv.Itoa(amd_slots))
 
-                    if pe_max > 127 {
-                        jsv.JSV_set_param("R", "y")
+                        if pe_max > 127 {
+                            jsv.JSV_set_param("R", "y")
+                        }
                     }
                 }
+                jsv.JSV_set_param("binding_step", "1")
+                modified_p = true
             }
-
-            jsv.JSV_set_param("binding_step", "1")
-            modified_p = true
         }
     }
 
+    jsv.JSV_show_params()
     if modified_p {
-        jsv.JSV_show_params()
         jsv.JSV_correct("Job was modified")
     } else {
         jsv.JSV_correct("Job was not modified")
