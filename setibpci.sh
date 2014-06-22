3ec8f27ddb97ab8385f491fd0e29d4a6c6dbdaa4
David Chin <david.w.h.chin@gmail.com>
2014-06-22 16:31:56 -0400

    Use numeric vendor ID instead of grepping

--- setibpci.sh
+++ setibpci.sh
@@ -3,10 +3,12 @@
 ### to a vendor-recommended setting.
 PATH=/bin:/usr/bin:/sbin:/usr/sbin
 
-pci_setting="68.W"
+# Mellanox vendor ID is 15b3
+vendor_id=15b3
+pci_setting=68.W
 
-mlnx_pci_id=$(lspci | grep -i mellanox | cut -f1 -d\ )
-max_read_req=$(setpci -s $mlnx_pci_id $pci_setting)
-new_max_read_req=$(echo $max_read_req | sed -e 's/^2/5/')
+mlnx_pci_id=$( lspci -d ${vendor_id}: | cut -f1 -d\  )
+max_read_req=$( setpci -s $mlnx_pci_id $pci_setting )
+new_max_read_req=$( echo $max_read_req | sed -e 's/^2/5/' )
 setpci -s $mlnx_pci_id $pci_setting=$new_max_read_req
 
