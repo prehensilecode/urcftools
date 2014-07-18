8949bb67100d9504210cdfc3d1d00917213c9ff3
David Chin <david.w.h.chin@gmail.com>
2014-07-18 14:07:07 -0400

    Deal with special case of pe_name = "shm"

--- pecheck.go
+++ pecheck.go
@@ -39,6 +39,9 @@ func job_verification_function() {
     } else {
         // parallel job
         if !jsv.JSV_is_param("binding_strategy") {
+
+            pe_name, _ := jsv.JSV_get_param("pe_name")
+
             var v string
             v, _ = jsv.JSV_get_param("pe_max")
             pe_max, _ := strconv.Atoi(v)
@@ -46,101 +49,112 @@ func job_verification_function() {
             v, _ = jsv.JSV_get_param("pe_min")
             pe_min, _ := strconv.Atoi(v)
 
-            l_hard, _ := jsv.JSV_get_param("l_hard")
-            q_hard, _ := jsv.JSV_get_param("q_hard")
-
-            singleat_re, _ := regexp.Compile("@[a-z]")
-            doubleat_re, _ := regexp.Compile("@@")
-            intelhost_re, _ := regexp.Compile("^ic[:digit:]{2}n[:digit:]{2}$")
-            amdhost_re, _ := regexp.Compile("^ac[:digit:]{2}n[:digit:]{2}$")
-            gpuhost_re, _ := regexp.Compile("^gpu[:digit:]{2}$")
-
-            var q string
-            var host string
-            var hostlist string
-
-            if singleat_re.MatchString(q_hard) {
-                // we have a single host
-                q = strings.Split(q_hard, "@")[0]
-                host = strings.Split(q_hard, "@")[1]
-            } else if doubleat_re.MatchString(q_hard) {
-                // we have a hostgroup
-                q = strings.Split(q_hard, "@")[0]
-                hostlist = strings.SplitAfterN(q_hard, "@", 2)[1]
-            } else {
-                // we have no host specifier
-                q = q_hard
-            }
-
-            cplx := make(map[string]string)
-            l_hard_split := strings.Split(l_hard, ",")
-            for _, v := range l_hard_split {
-                cplx[strings.Split(v, "=")[0]] = strings.Split(v, "=")[1]
-            }
-
-            vendor, ok := cplx["vendor"]
-
-            if !ok {
-                // vendor was not requested by job
-                if strings.EqualFold("gpu.q", q) || strings.EqualFold("@intelhosts", hostlist) {
-                    vendor = "intel"
-                } else if strings.EqualFold("@amdhosts", hostlist) {
-                    vendor = "amd"
-                } else if len(host) != 0 {
-                    // specific hosts requested
-                    if intelhost_re.MatchString(host) || gpuhost_re.MatchString(host) {
-                        vendor = "intel"
-                    } else if amdhost_re.MatchString(host) {
-                        vendor = "amd"
-                    }
-                } else {
-                    _, ok = cplx["ngpus"]
-                    if !ok {
-                        _, ok = cplx["gpu"]
-                    }
-                    
-                    if ok {
-                        vendor = "intel"
-                    }
-                }
-            }
-
-
-            // Only specify binding if pe_max == pe_min, i.e. if a fixed
-            // number of slots is requested
-
-            if pe_max == pe_min {
+            if strings.EqualFold("shm", pe_name) && (pe_max == pe_min) {
                 jsv.JSV_set_param("binding_strategy", "striding_automatic")
                 jsv.JSV_set_param("binding_type", "set")
+                jsv.JSV_set_param("binding_amount", strconv.Itoa(pe_max))
+                jsv.JSV_set_param("binding_step", "1")
+                modified_p = true
+            } else {
 
-                if strings.EqualFold("intel", vendor) {
-                    if pe_max < intel_slots {
-                        jsv.JSV_set_param("binding_amount", strconv.Itoa(pe_max))
+                l_hard, _ := jsv.JSV_get_param("l_hard")
+                q_hard, _ := jsv.JSV_get_param("q_hard")
+
+                singleat_re, _ := regexp.Compile("@[a-z]")
+                doubleat_re, _ := regexp.Compile("@@")
+                intelhost_re, _ := regexp.Compile("^ic[:digit:]{2}n[:digit:]{2}$")
+                amdhost_re, _ := regexp.Compile("^ac[:digit:]{2}n[:digit:]{2}$")
+                gpuhost_re, _ := regexp.Compile("^gpu[:digit:]{2}$")
+
+                var q string
+                var host string
+                var hostlist string
+
+                if singleat_re.MatchString(q_hard) {
+                    // we have a single host
+                    q = strings.Split(q_hard, "@")[0]
+                    host = strings.Split(q_hard, "@")[1]
+                } else if doubleat_re.MatchString(q_hard) {
+                    // we have a hostgroup
+                    q = strings.Split(q_hard, "@")[0]
+                    hostlist = strings.SplitAfterN(q_hard, "@", 2)[1]
+                } else {
+                    // we have no host specifier
+                    q = q_hard
+                }
+
+                cplx := make(map[string]string)
+                l_hard_split := strings.Split(l_hard, ",")
+                for _, v := range l_hard_split {
+                    cplx[strings.Split(v, "=")[0]] = strings.Split(v, "=")[1]
+                }
+
+                vendor, ok := cplx["vendor"]
+
+                if !ok {
+                    // vendor was not requested by job
+                    if strings.EqualFold("gpu.q", q) || strings.EqualFold("@intelhosts", hostlist) {
+                        vendor = "intel"
+                    } else if strings.EqualFold("@amdhosts", hostlist) {
+                        vendor = "amd"
+                    } else if len(host) != 0 {
+                        // specific hosts requested
+                        if intelhost_re.MatchString(host) || gpuhost_re.MatchString(host) {
+                            vendor = "intel"
+                        } else if amdhost_re.MatchString(host) {
+                            vendor = "amd"
+                        }
                     } else {
-                        jsv.JSV_set_param("binding_amount", strconv.Itoa(intel_slots))
-                    }
+                        _, ok = cplx["ngpus"]
+                        if !ok {
+                            _, ok = cplx["cuda"]
+                        }
 
-                    if pe_max > 31 {
-                        jsv.JSV_set_param("R", "y")
-                    }
-                } else if strings.EqualFold("amd", vendor) {
-                    if pe_max < amd_slots {
-                        jsv.JSV_set_param("binding_amount", strconv.Itoa(pe_max))
-                    } else {
-                        jsv.JSV_set_param("binding_amount", strconv.Itoa(amd_slots))
-
-                        if pe_max > 127 {
-                            jsv.JSV_set_param("R", "y")
+                        if ok {
+                            vendor = "intel"
+                        } else {
+                            vendor = "undef"
                         }
                     }
                 }
-                jsv.JSV_set_param("binding_step", "1")
-                modified_p = true
+
+
+                // Only specify binding if pe_max == pe_min, i.e. if a fixed
+                // number of slots is requested
+
+                if !strings.EqualFold("undef", vendor) && pe_max == pe_min {
+                    jsv.JSV_set_param("binding_strategy", "striding_automatic")
+                    jsv.JSV_set_param("binding_type", "set")
+
+                    if strings.EqualFold("intel", vendor) {
+                        if pe_max < intel_slots {
+                            jsv.JSV_set_param("binding_amount", strconv.Itoa(pe_max))
+                        } else {
+                            jsv.JSV_set_param("binding_amount", strconv.Itoa(intel_slots))
+                        }
+
+                        if pe_max > 31 {
+                            jsv.JSV_set_param("R", "y")
+                        }
+                    } else if strings.EqualFold("amd", vendor) {
+                        if pe_max < amd_slots {
+                            jsv.JSV_set_param("binding_amount", strconv.Itoa(pe_max))
+                        } else {
+                            jsv.JSV_set_param("binding_amount", strconv.Itoa(amd_slots))
+
+                            if pe_max > 127 {
+                                jsv.JSV_set_param("R", "y")
+                            }
+                        }
+                    }
+                    jsv.JSV_set_param("binding_step", "1")
+                    modified_p = true
+                }
             }
         }
     }
 
-    //jsv.JSV_show_params()
+    jsv.JSV_show_params()
     if modified_p {
         jsv.JSV_correct("Job was modified")
     } else {
