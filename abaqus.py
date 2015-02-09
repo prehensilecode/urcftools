6f2dcdbf3d1774f5b562df48f58dd0fa04196b53
David Chin <david.w.h.chin@gmail.com>
2015-02-09 11:14:22 -0500

    Abaqus PE script added

---
+++ abaqus.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+import sys, os
+
+### PE startup script aka prologue to set up Abaqus MPI "hostfile"
+### Based on documented env file format
+###     http://www.simulia.com/support/v67/books/sgb67EF/default.htm?startat=ch04s01.html
+
+machinefile = os.environ['PE_HOSTFILE']
+abaqenvfile = "abaqus_v6.env"
+
+machinelines = []
+with open(machinefile, "ro") as machinefile:
+    for l in machinefile:
+        lsplit = l.split()
+        machinelines.append( [lsplit[0], int(lsplit[1])] )
+
+with open(abaqenvfile, "wo") as envfile:
+    envfile.write("mp_mode=MPI\n")
+    envfile.write("mp_host_list=%s\n" % (str(machinelines)))
+
