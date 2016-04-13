666b80e2026d1b4c9c48ac03a1eb96df0fff952b
David Chin <david.w.h.chin@gmail.com>
2016-04-13 12:29:54 -0400

    Added Drexel IRT license servers to LM_LICENSE_FILE

--- lmcheck.sh
+++ lmcheck.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
-export FLEXLMDIR=/opt/flexlm
+#export FLEXLMDIR=/opt/flexlm
+export FLEXLMDIR=/cm/shared/apps/flexlm/11.13
 export PATH=${FLEXLMDIR}/bin:${PATH}
-export LM_LICENSE_FILE=28518@proteusmaster.cm.cluster:27495@license01.coe.drexel.edu:27495@license02.coe.drexel.edu:27495@license03.coe.drexel.edu
+export LM_LICENSE_FILE=28518@proteusmaster.cm.cluster:27495@license01.coe.drexel.edu:27495@license02.coe.drexel.edu:27495@license03.coe.drexel.edu:1711@oystercatcher.irt.drexel.edu:1711@goose.irt.drexel.edu:1711@killdeer.irt.drexel.edu
+
 
 
 WWWHOME=/var/www/html
 STATUSFILE=${WWWHOME}/lmstat.txt
 STATUSFILEHTML=${WWWHOME}/lmstat.html
 
-lmstat -a | grep -v "License\ file" | sed -e 's/27495/...../g' | sed -e 's/28518/...../g' > ${STATUSFILE}
+lmutil lmstat -a | grep -v "License\ file" | sed -e 's/27495/./g' | sed -e 's/28518/./g' > ${STATUSFILE}
 
 chmod 644 ${STATUSFILE}
 
@@ -18,7 +20,7 @@ echo "     \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\">" >> ${ST
 echo "     <html xmlns=\"http://www.w3.org/1999/xhtml\" xml:lang=\"en\" lang=\"en\">" >> ${STATUSFILEHTML}
 echo "     <head>" >> ${STATUSFILEHTML}
 echo "         <meta http-equiv=\"Content-type\" content=\"text/html;charset=UTF-8\" />" >> ${STATUSFILEHTML}
-echo "         <meta http-equiv="refresh" content=\"300\"/>" >> ${STATUSFILEHTML}
+echo "         <meta http-equiv="refresh" content=\"600\"\>" >> ${STATUSFILEHTML}
 echo "         <title>License Status</title>" >> ${STATUSFILEHTML}
 echo "     </head>" >> ${STATUSFILEHTML}
 echo "<body>" >> ${STATUSFILEHTML}
