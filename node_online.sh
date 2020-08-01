bba014d5cccd84c920a1615d3797a10f3f5b6871
David Chin <david.w.h.chin@gmail.com>
2020-08-01 13:54:02 -0400

    Make able to do all intel, amd, or gpu nodes

--- node_online.sh
+++ node_online.sh
@@ -16,11 +16,38 @@ then
     exit 1
 fi
 
-for node in "$@"
-do
-    for qc in $( qstat -f | grep -v ^- | grep -v ^queuename | grep @$node | awk '{print $1}' )
-    do
-        qmod -e $qc
-    done
-done
+declare -a inarr=($@)
 
+if  [ ${#inarr[@]} -eq 1 ]
+then
+    if [ ${inarr[0]} = intel ]
+    then
+         for i in $( seq -w 1 22 )
+         do
+             crate_online.sh ic${i}
+         done
+    elif [ ${inarr[0]} = amd ]
+    then
+        for i in $( seq 1 6 ) 
+        do
+            crate_online.sh ac0${i}
+        done
+    elif [ ${inarr[0]} = gpu ]
+    then
+        for i in $( seq 1 8 )
+        do
+            crate_online.sh gpu0${i}
+        done
+    else
+        echo "Only 'intel', 'amd', and 'gpu' are recognized"
+        exit 1
+    fi
+else
+    for node in $inarr
+    do
+        for qc in $( qstat -f | grep -v ^- | grep -v ^queuename | grep @$node | awk '{print $1}' )
+        do
+            qmod -d $qc
+        done
+    done
+fi
